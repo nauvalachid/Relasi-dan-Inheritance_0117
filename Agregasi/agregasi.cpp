e3725132b364bb1a1e028eaa8513c5c60a357c27
nauvalachid <nauvalachid@gmail.com>
2025-05-27 18:39:24 +0700

    Menambahkan relasi antara objek Ibu dan Anak serta mencetak data anak dari masing-masing ibu

---
+++ Agregasi/agregasi.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include "ibu.h"
+#include "anak.h"
+#include <string>
+#define IBU_H
+using namespace std;
+
+#include "ibu.h"
+#include "anak.h"
+
+int main() {
+    ibu* varIbu = new ibu("Dini");
+    ibu* varIbu2 = new ibu("Novi");
+    anak* varAnak1 = new anak("Tono");
+    anak* varAnak2 = new anak("Rini");
+    anak* varAnak3 = new anak("Dewi");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
+
+    varIbu->cetakAnak();
+    varIbu2->cetakAnak();
+
+    delete varIbu;
+    delete varIbu2;
+    delete varAnak1;
+    delete varAnak2;
+    delete varAnak3;
+}
