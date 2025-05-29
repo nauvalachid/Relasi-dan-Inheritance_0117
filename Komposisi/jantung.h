5d8458b75cc137b618d247addea876f74a9d1d58
nauvalachid <nauvalachid@gmail.com>
2025-05-29 21:04:03 +0700

    Menambahkan kelas jantung dengan konstruktor dan destruktor yang menampilkan pesan saat dihidupkan dan dimatikan

--- Komposisi/jantung.h
+++ Komposisi/jantung.h
@@ -1,6 +1,17 @@
+#include <iostream>
 #ifndef JANTUNG_H
 #define JANTUNG_H
 
 using namespace std;
 
+class jantung {
+public:
+    jantung() {
+        cout << "Jantung dihidupkan\n" << endl;
+    }
+
+    ~jantung() {
+        cout << "Jantung dimatikan\n" << endl;
+    }
+};
 #endif
